487be12bea6bd57badb4968373d364501995d743
yuanpj <b03901134@ntu.edu.tw>
2019-10-10 18:09:42 +0800

    Modify Dataset Class, save_checkpoint fn

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 mkdir -p log/$2
 cp $1 log/$2
 export CUDA_VISIBLE_DEVICES=$3
-python3 main.py --config $1 --name $1 --logdir log --ckpdir checkpoint --outdir save --njobs 10 --seed 0 $4
+python3 main.py --config $1 --name $2 --logdir log --ckpdir checkpoint --outdir save --njobs 10 --seed 0 $4
